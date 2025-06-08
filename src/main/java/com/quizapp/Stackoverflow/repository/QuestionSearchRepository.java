3b7f8f9daf6115740be1162565de24737a8c57d3
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-08 22:11:51 +0300

    elasticsearch

--- src/main/java/com/quizapp/Stackoverflow/repository/QuestionSearchRepository.java
+++ src/main/java/com/quizapp/Stackoverflow/repository/QuestionSearchRepository.java
@@ -3,5 +3,9 @@ package com.quizapp.Stackoverflow.repository;
 import com.quizapp.Stackoverflow.model.QuestionDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface QuestionSearchRepository extends ElasticsearchRepository<QuestionDocument,Long> {
+
+    List<QuestionDocument> findByTitleContainingIgnoreCase(String title);
 }
