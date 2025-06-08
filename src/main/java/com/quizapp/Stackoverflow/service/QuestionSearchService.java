e7cadaaf50d776f431a163248e4f0d574381f5d8
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-08 22:12:31 +0300

    elasticsearch

---
+++ src/main/java/com/quizapp/Stackoverflow/service/QuestionSearchService.java
@@ -0,0 +1,41 @@
+package com.quizapp.Stackoverflow.service;
+
+package com.quizapp.Stackoverflow.elasticsearch;
+
+import com.quizapp.Stackoverflow.model.Question;
+import com.quizapp.Stackoverflow.model.QuestionDocument;
+import com.quizapp.Stackoverflow.model.Tag;
+import com.quizapp.Stackoverflow.repository.QuestionSearchRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+
+public class QuestionSearchService {
+
+    private final QuestionSearchRepository searchRepository;
+
+    public QuestionSearchService(QuestionSearchRepository searchRepository) {
+        this.searchRepository = searchRepository;
+    }
+
+    public List<QuestionDocument> searchByTitle(String keyword) {
+        return searchRepository.findByTitleContainingIgnoreCase(keyword);
+    }
+
+    public void indexQuestion(Question question) {
+        QuestionDocument doc = new QuestionDocument();
+        doc.setId(question.getId());
+        doc.setTitle(question.getTitle());
+        doc.setContent(question.getContent());
+        doc.setAuthor(question.getAuthor().getUsername());
+        doc.setTags(question.getTags().stream().map(Tag::getName).collect(Collectors.toList()));
+        doc.setCreatedAt(question.getCreatedAt());
+
+        searchRepository.save(doc);
+    }
+
+
+}
