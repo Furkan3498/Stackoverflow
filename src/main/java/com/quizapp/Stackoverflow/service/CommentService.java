bdd8dcb2c09e3933a87d63d99d8f6bd30b4fa57b
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-10 21:51:58 +0300

    refactoring

--- src/main/java/com/quizapp/Stackoverflow/service/CommentService.java
+++ src/main/java/com/quizapp/Stackoverflow/service/CommentService.java
@@ -3,28 +3,28 @@ package com.quizapp.Stackoverflow.service;
 
 
 import com.quizapp.Stackoverflow.dto.CommentRequestDTO;
-import com.quizapp.Stackoverflow.dtoResponse.CommentResponseDTO;
-import com.quizapp.Stackoverflow.mapper.CommentMapper;
 import com.quizapp.Stackoverflow.model.Answer;
 import com.quizapp.Stackoverflow.model.Comment;
 import com.quizapp.Stackoverflow.model.Question;
-import com.quizapp.Stackoverflow.model.User;
 import com.quizapp.Stackoverflow.repository.AnswerRepository;
 import com.quizapp.Stackoverflow.repository.CommentRepository;
 import com.quizapp.Stackoverflow.repository.QuestionRepository;
-import com.quizapp.Stackoverflow.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
-@RequiredArgsConstructor
+
 public class CommentService {
     private final CommentRepository commentRepository;
     private final AnswerRepository answerRepository;
     private final QuestionRepository questionRepository;
     private final UserService userService;
+
+    public CommentService(CommentRepository commentRepository, AnswerRepository answerRepository, QuestionRepository questionRepository, UserService userService) {
+        this.commentRepository = commentRepository;
+        this.answerRepository = answerRepository;
+        this.questionRepository = questionRepository;
+        this.userService = userService;
+    }
 
     public Comment commentOnQuestion(Long questionId, CommentRequestDTO dto) {
         Question question = questionRepository.findById(questionId)
