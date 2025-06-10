6aac03d74e70bcb04d3dcfc1c776dad476436e8f
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-10 21:46:22 +0300

    elasticsearch

--- src/main/java/com/quizapp/Stackoverflow/service/QuestionService.java
+++ src/main/java/com/quizapp/Stackoverflow/service/QuestionService.java
@@ -1,31 +1,25 @@
 package com.quizapp.Stackoverflow.service;
 
-
 import com.quizapp.Stackoverflow.dto.QuestionRequestDTO;
-import com.quizapp.Stackoverflow.dtoResponse.QuestionResponseDTO;
-import com.quizapp.Stackoverflow.mapper.QuestionMapper;
 import com.quizapp.Stackoverflow.model.Question;
 import com.quizapp.Stackoverflow.model.Tag;
-import com.quizapp.Stackoverflow.model.User;
 import com.quizapp.Stackoverflow.repository.QuestionRepository;
 import com.quizapp.Stackoverflow.repository.TagRepository;
-import com.quizapp.Stackoverflow.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
+
     private final QuestionRepository questionRepository;
     private final TagRepository tagRepository;
     private final UserService userService;
+    private final QuestionSearchService questionSearchService;
 
     public Question createQuestion(QuestionRequestDTO dto) {
         Question question = new Question();
@@ -39,7 +33,12 @@ public class QuestionService {
                 .collect(Collectors.toList());
         question.setTags(tags);
 
-        return questionRepository.save(question);
+        Question saved = questionRepository.save(question);
+
+
+        questionSearchService.indexQuestion(saved);
+
+        return saved;
     }
 
     public List<Question> getAllQuestions() {
