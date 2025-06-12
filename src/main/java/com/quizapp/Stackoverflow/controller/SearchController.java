061572141c23587946e86d5bdb7e81ffb93aa202
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-13 00:54:22 +0300

    refactoring

--- src/main/java/com/quizapp/Stackoverflow/controller/SearchController.java
+++ src/main/java/com/quizapp/Stackoverflow/controller/SearchController.java
@@ -4,18 +4,21 @@ package com.quizapp.Stackoverflow.controller;
 
 import com.quizapp.Stackoverflow.model.QuestionDocument;
 import com.quizapp.Stackoverflow.service.QuestionSearchService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
+
 @RequestMapping("/api/search")
 public class SearchController {
 
     private final QuestionSearchService searchService;
+
+    public SearchController(QuestionSearchService searchService) {
+        this.searchService = searchService;
+    }
 
     @GetMapping("/questions")
     public ResponseEntity<List<QuestionDocument>> searchQuestions(@RequestParam String keyword) {
