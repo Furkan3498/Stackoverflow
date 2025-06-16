8062a7ba55aa6a3c7dbf30065be0714f067a690b
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-06-16 22:42:29 +0300

    websocket

---
+++ src/main/java/com/quizapp/Stackoverflow/dto/NotificationDTOWebSocket.java
@@ -0,0 +1,39 @@
+package com.quizapp.Stackoverflow.dto;
+
+public class NotificationDTOWebSocket { private Long id;
+    private String message;
+    private String type;
+    private boolean read;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+}
